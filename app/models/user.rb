f469ce4baa791c72b39b3c85a1d930e3cf4101ba
wley3337 <wley3337@gmail.com>
2018-07-09 16:28:43 -0400

    added password field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :listings, foreign_key: "owner_id"
   has_many :exchanges, through: :listings, foreign_key: "owner_id"
   has_many :exchanges, foreign_key: "renter_id"
-
+  has_secure_password 
   #we write a custom method to get renter info by forcing the key asssociation
 end
