4d81f964548b001fc84a40213c8fbbf24a3a793f
wley3337 <wley3337@gmail.com>
2018-07-10 15:36:11 -0400

    added time format helpers to Exchanges

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,13 @@
 class Exchange < ApplicationRecord
   belongs_to :renter, class_name: "User"
   belongs_to :listing
+
+  def start_date_format
+    self.start_date.strftime("%a, %B %e, %Y")
+  end
+
+  def start_date_format
+    self.end_date.strftime("%a, %B %e, %Y")
+  end
+
 end
