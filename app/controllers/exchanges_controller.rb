d4b2ffd036b41b2838f44524ea4bed80a3fad90c
github username <github email address>
2018-07-10 17:35:55 -0400

    Exchange show page

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,2 +1,13 @@
 class ExchangesController < ApplicationController
+
+  def show
+    @exchange = Exchange.find(params[:id ])
+
+    @renter = User.find(@exchange.renter_id)
+
+    @listing = Listing.find(@exchange.listing_id)
+    @owner = User.find(@listing.owner_id)
+  end
+
+  
 end
--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,47 @@
 class Exchange < ApplicationRecord
   belongs_to :renter, class_name: "User"
   belongs_to :listing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def get_owner_name
+    id = self.listing.owner_id
+    owner = User.find(id)
+    owner.name
+  end
+
+
+  def get_renter_name
+    id = self.renter_id
+    renter = User.find(id)
+    renter.name
+  end
+
+
+  def get_duration
+    ((self.end_date).to_date - (self.start_date).to_date).to_i
+  end
+
+
+  def get_rental_cost
+    listing = Listing.find(self.listing_id)
+    rental_per_day = listing.rental_price
+
+    rental_cost = rental_per_day * self.get_duration.to_f
+    # number_to_currency(rental_cost)
+  end
+
+
 end
