90ffbab1c8dd6926baebec2c3bbbdcdc208a5a72
github username <github email address>
2018-07-12 12:02:45 -0400

    Added tests for logged in user - not fully functioning :(

--- spec/controllers/exchanges_controller_spec.rb
+++ spec/controllers/exchanges_controller_spec.rb
@@ -2,27 +2,41 @@ require 'rails_helper'
 require 'rake'
 RSpec.describe ExchangesController, type: :controller do
 
-
-
   describe 'create exchange' do
-      before(:each) do
-        u1 = User.create(name: "Essa", username: "e1", password: "essa123")
-        u2 = User.create(name: "Anthony", username: "a1", password: "anthony123")
-        l1 = Listing.create(product_name: "Bike", product_description: "bicycle", item_value: 1000.00, rental_price: 30.00, buy_price: 800.00, quality: "Mint Condition", address: "123 Street Rd.", zip_code: 20009)
-        u1.listings << l1
-      end
+    before(:each) do
 
 
-      it 'can not create exchange with end_date < start_date'do
-       post :create, params: { exchange: {renter_id: 2, listing_id: 1, start_date: "2023-10-24", end_date: "2022-10-25", total_price: 10.00} }
-        expect(response).to render_template(:new)
-      end
+      @u1 = User.create(name: "Essa", username: "e1", password: "essa123")
+      @u2 = User.create(name: "Anthony", username: "a1", password: "anthony123")
+      @u3 = User.create(name: "Will", username: "w1", password: "will123")
+
+      @l1 = Listing.create(product_name: "Bike", product_description: "bicycle", item_value: 1000.00, rental_price: 30.00, buy_price: 800.00, quality: "Mint Condition", address: "123 Street Rd.", zip_code: 20009)
+      @u1.listings << @l1
+
     end
+
+
+    it 'can not create exchange with end_date < start_date'do
+     post :create, params: { exchange: {renter_id: 2, listing_id: 1, start_date: "2023-10-24", end_date: "2022-10-25", total_price: 10.00} }
+      expect(response).to render_template(:new)
+    end
+
+
+    it 'user cannot rent own item' do
+      post :create, params: { exchange: {renter_id: @u1.id, listing_id: @l1.id, start_date: "2023-10-24", end_date: "2024-10-25", total_price: 10.00}}
+        expect(response).to redirect_to(user_path(@u1))
+    end
+
+    it 'user cannot view exchange if not part of it' do
+      get :show, p  arams: { exchange: {renter_id: @u2.id, listing_id: @l1.id, start_date: "2023-10-24", end_date: "2024-10-25", total_price: 10.00}}, session: {user_id: @u3.id}
+      expect(response).to redirect_to(user_path(@u3))
+    end
+
+  end
+
+
+
 end
-#
-
-
-
 
 # RSpec.describe UsersController, type: :controller do
 #
@@ -56,26 +70,3 @@ end
 
 
 # before { create(:widget) }
-
-
-  describe 'create exchange' do
-    before(:each) do
-
-
-      u1 = User.create(name: "Essa", username: "e1", password: "essa123")
-
-      u2 = User.create(name: "Anthony", username: "a1", password: "anthony123")
-      l1 = Listing.create(product_name: "Bike", product_description: "bicycle", item_value: 1000.00, rental_price: 30.00, buy_price: 800.00, quality: "Mint Condition", address: "123 Street Rd.", zip_code: 20009)
-
-      u1.listings << l1
-    end
-
-
-    it 'can not create exchange with end_date < start_date'do
-     post :create, params: { exchange: {renter_id: 2, listing_id: 1, start_date: "2023-10-24", end_date: "2022-10-25", total_price: 10.00} }
-      expect(response).to render_template(:new)
-    end
-  end
-
-
-end
