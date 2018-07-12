0f5974e132eb969131edd2fe7c569db70f14bab2
github username <github email address>
2018-07-12 13:51:54 -0400

    blah

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -14,14 +14,17 @@ RSpec.describe Listing, type: :model do
             @u2 = User.create(name: "Henry", username: "Henry1", password: "password")
             @listing = Listing.create(product_name: "Laptop", product_description: "Mac Book Pro", item_value: 2000.00, rental_price: 25.00, buy_price: 1700.00, quality: "Slow", address: "213 Street Rd.", zip_code: 20009)
             @u1.listings << @listing
-            @exchange = Exchange.create(renter_id: @u2.id, listing_id: @listing.id, start_date: "1997-10-21", end_date: "1997-10-24")
-            
+            @exchange = Exchange.new(renter_id: @u2.id, listing_id: @listing.id, start_date: "2018-10-21", end_date: "2018-10-24")
+            @exchange.total_price = @exchange.get_rental_cost
+            @exchange.save
+        
             
             
         end
 
        it 'should return true if dates confilct' do
-            expect(@listing.date_conflict?("1997-09-10", "1997-10-15")).to eq(true)
+
+            expect(@listing.date_conflict?("2018-10-23", "2018-10-26")).to eq(true)
        end
     end
 end
