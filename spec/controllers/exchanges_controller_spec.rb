a7eb4716077aae592b7df880317197c78e544160
github username <github email address>
2018-07-12 10:07:01 -0400

    Started adding test cases

--- spec/controllers/exchanges_controller_spec.rb
+++ spec/controllers/exchanges_controller_spec.rb
@@ -2,21 +2,28 @@ require 'rails_helper'
 
 RSpec.describe ExchangesController, type: :controller do
 
-  let(:exchange_fail) {{renter_id: 1, listing_id: 2, start_date: 10-21-1997, end_date: 10-24-1996}}
-
-  let(:exchange_test_2) {{renter_id: 2, listing_id: 3, start_date: 10-21-2023, end_date: 10-24-2023}}
 
 
   describe 'create exchange' do
+      before(:each) do
+        u1 = User.create(name: "Essa", username: "e1", password: "essa123")
+        u2 = User.create(name: "Anthony", username: "a1", password: "anthony123")
+        l1 = Listing.create(product_name: "Bike", product_description: "bicycle", item_value: 1000.00, rental_price: 30.00, buy_price: 800.00, quality: "Mint Condition", address: "123 Street Rd.", zip_code: 20009)
+        u1.listings << l1
+      end
 
-    it 'can not create exchange with end_date < start_date'do
-     post :create, :exchange =>  {renter_id: 1, listing_id: 2, start_date: 10-21-1997, end_date: 10-24-1996}
-      expect(response).to render_template(:new)
+
+      it 'can not create exchange with end_date < start_date'do
+       post :create, params: { exchange: {renter_id: 2, listing_id: 1, start_date: "2023-10-24", end_date: "2022-10-25", total_price: 10.00} }
+        expect(response).to render_template(:new)
+      end
     end
-  end
-
 end
 #
+
+
+
+
 # RSpec.describe UsersController, type: :controller do
 #
 #   let(:steven) {{name: 'Steven',
