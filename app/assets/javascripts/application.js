f605ed896f2f215487daa28e56b1c8ec1c977127
github username <github email address>
2018-07-13 12:09:53 -0400

    styled listings index page and created navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,6 @@
 //= require_tree .
 
 // required for bootstrap
-= require jquery3
-= require popper
-= require bootstrap-sprockets
+//= require jquery3
+// = require popper
+// = require bootstrap-sprockets
